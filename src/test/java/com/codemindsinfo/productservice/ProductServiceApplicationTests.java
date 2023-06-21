bd6d0e4ab5ab1263a117e2fb69d4363576dead09
Marisetty <mvenkatasai@gmail.com>
2023-06-21 23:39:18 +0530

    Committing the code changes for Product Service

--- src/test/java/com/codemindsinfo/productservice/ProductServiceApplicationTests.java
+++ src/test/java/com/codemindsinfo/productservice/ProductServiceApplicationTests.java
@@ -1,15 +1,64 @@
 package com.codemindsinfo.productservice;
 
+import com.codemindsinfo.productservice.model.ProductRequest;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+
+import java.math.BigDecimal;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @Testcontainers
+@AutoConfigureMockMvc
 class ProductServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Container
+    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:4.4.2");
+
+    @Autowired
+    MockMvc mockMvc;
+    @Autowired
+    ObjectMapper objectMapper;
+
+
+    static void setProperties(DynamicPropertyRegistry dynamicPropertyRegistry) {
+        dynamicPropertyRegistry.add("spring.data.mongodb.uri", mongoDBContainer::getReplicaSetUrl);
+    }
+
+    @Test
+    void shouldCreateProduct() throws Exception {
+
+        ProductRequest productRequest = getProductRequest();
+        String productRequestAsString = getProductReqAsString(productRequest);
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/product")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(productRequestAsString))
+                .andExpect(status().isCreated());
+    }
+
+    private String getProductReqAsString(ProductRequest productRequest) throws JsonProcessingException {
+        return new ObjectMapper().writeValueAsString(productRequest);
+    }
+
+    private ProductRequest getProductRequest() {
+        return ProductRequest.builder()
+                .productName("iPhone 13")
+                .price(BigDecimal.valueOf(1200))
+                .description("Made in India from Test")
+                .build();
+    }
+
 
 }
